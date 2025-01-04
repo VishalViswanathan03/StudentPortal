a95f78ac3ce03527a3594591765960101c50c5ef
VishalViswanathan03 <vishalvn2003@gmail.com>
2025-01-04 20:52:49 +0530

    Added necessory deleted files, added view for adding student information.

---
+++ StudentPortal/Controllers/StudentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
