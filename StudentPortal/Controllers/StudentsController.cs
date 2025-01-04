73be88d55f2f805e4230b936a075a0c6a86276b5
VishalViswanathan03 <vishalvn2003@gmail.com>
2025-01-04 21:42:19 +0530

    Added Add student functionality.

--- StudentPortal/Controllers/StudentsController.cs
+++ StudentPortal/Controllers/StudentsController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
+using StudentPortal.Models;
+using StudentPortal.Models.Entities;
 
 namespace StudentPortal.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public StudentsController(ApplicationDbContext dbcontext)
+        {
+            _dbContext=dbcontext;   
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Students
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+
+            await _dbContext.Students.AddAsync(student);
+            await _dbContext.SaveChangesAsync();
+
             return View();
         }
 
